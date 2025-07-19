c02d2faaf91596636df9d6db2000b9bee9aa2da8
Nasibullah Bahir <144516590+Nasibullahbahir@users.noreply.github.com>
2025-07-19 12:39:16 +0430

    Update startScreen.js
    add num Question

--- src/components/startScreen.js
+++ src/components/startScreen.js
@@ -2,7 +2,7 @@ function StartScreen({ numQuestions, dispatch }) {
   return (
     <div className="start">
       <h2>Welcome to The Raect Quize!</h2>
-      <h3>X question to test React mastary</h3>
+      <h3>{numQuestions + 1} question to test React mastary</h3>
       <button
         className="btn btn-ui"
         onClick={() => dispatch({ type: "start" })}
